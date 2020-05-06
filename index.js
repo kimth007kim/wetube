108f1efeffb75e920d496e4e6856e55e7d7f1b57
kimth007kim <kimth007kim@naver.com>
2020-05-06 21:51:47 +0900

    #2.4 Handling Routes with Express 완료

--- index.js
+++ index.js
@@ -6,4 +6,16 @@ const PORT = 4000;
 function handleListening() {
   console.log(`Listening on: http://localhost:${PORT}`);
 }
+function handleHome(req, res) {
+  //   console.log(req);
+  res.send("Hello from home");
+}
+function handleProfile(req, res) {
+  res.send("you are on my profile");
+}
+
+app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
+
 app.listen(PORT, handleListening);
