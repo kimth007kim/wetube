e5e33e5c54ae95c29d0d82dde00cbee8c6d79889
kimth007kim <kimth007kim@naver.com>
2020-05-06 20:55:48 +0900

    #2.3 Your First Express Server 완료

---
+++ README.md
@@ -0,0 +1,1 @@
+Cloning youtube with vanilla javascript
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("hi");
+const express = require("express");
+const app = express();
+
+const PORT = 4000;
+
+function handleListening() {
+  console.log(`Listening on: http://localhost:${PORT}`);
+}
+app.listen(PORT, handleListening);
