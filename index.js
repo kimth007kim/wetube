ff0b11aeac7b5a941b05ed85140e8dad42bf22c0
kimth007kim <kimth007kim@naver.com>
2020-05-07 16:18:27 +0900

    #2.6 Express Core: Middlewares 완료

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ const handleHome = (req, res) => res.send("Hello from home");
 
 const handleProfile = (req, res) => res.send("you are on my profile");
 
+const betweenHome = (req, res, next) => {
+  console.log("I'm between");
+  next();
+};
+app.use(betweenHome);
+
 app.get("/", handleHome);
 
 app.get("/profile", handleProfile);
