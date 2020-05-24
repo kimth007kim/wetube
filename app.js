f872e54ecfbfdf4c1c4ca0b98a7c566b274ec06e
kimth007kim <kimth007kim@naver.com>
2020-05-24 13:45:35 +0900

    #2.13 Installing pug

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import globalRouter from "./routers/globalRouter";
 import routes from "./routes";
 const app = express();
 
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
