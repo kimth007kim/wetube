cca06c5635e60a749abff8be8cce8f2fed9c6fea
kimth007kim <kimth007kim@naver.com>
2020-06-12 18:49:46 +0900

    #3.5 Home Controller Finished

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -21,12 +21,18 @@ export const search = (req, res) => {
 export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
 
-export const postUpload = (req, res) => {
+export const postUpload = async (req, res) => {
   const {
     body: { file, title, description },
+    file: { path },
   } = req;
-  //To Do:Upload and save video
-  res.redirect(routes.videoDetail(56448611552423));
+  const newVideo = await Video.create({
+    fileUrl: path,
+    title,
+    description,
+  });
+  console.log(newVideo);
+  res.redirect(routes.videoDetail(newVideo.id));
 };
 
 export const videoDetail = (req, res) =>
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -8,12 +8,13 @@ import {
   getUpload,
   postUpload,
 } from "../controllers/videoController";
+import { uploadVideo } from "../middlewares";
 
 const videoRouter = express.Router();
 
 // videoRouter.get(routes.videos, videos);
 videoRouter.get(routes.upload, getUpload);
-videoRouter.post(routes.upload, postUpload);
+videoRouter.post(routes.upload, uploadVideo, postUpload);
 videoRouter.get(routes.videoDetail(), videoDetail);
 videoRouter.get(routes.editVideo, editVideo);
 videoRouter.get(routes.deleteVideo, deleteVideo);
